7f1bea323f358ae8ea63662bdd6b3b28c7b23ea6
My Name <hishamrehab309@gmail.com>
2024-05-14 14:50:06 +0200

    set environment tools

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,19 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCwPEnaxs_0naiVlLMqOLDL6EH5kF8yJeA",
-  authDomain: "fir-cc5c5.firebaseapp.com",
-  projectId: "fir-cc5c5",
-  storageBucket: "fir-cc5c5.appspot.com",
-  messagingSenderId: "1095123270074",
-  appId: "1:1095123270074:web:64f793f50569524b180583",
-  measurementId: "G-YSCD2ET45Z",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
