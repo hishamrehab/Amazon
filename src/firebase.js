87d8fdea308d95408bce48bf0c35334ffa03bc01
My Name <hishamrehab309@gmail.com>
2024-05-14 13:49:25 +0200

    setup firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCwPEnaxs_0naiVlLMqOLDL6EH5kF8yJeA",
+  authDomain: "fir-cc5c5.firebaseapp.com",
+  projectId: "fir-cc5c5",
+  storageBucket: "fir-cc5c5.appspot.com",
+  messagingSenderId: "1095123270074",
+  appId: "1:1095123270074:web:64f793f50569524b180583",
+  measurementId: "G-YSCD2ET45Z",
+};
